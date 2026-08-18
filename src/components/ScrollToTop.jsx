e0bbd1d9f602d1890978082123d25317b731a306
IALTAIR <yferoze@gmail.com>
2026-08-18 19:36:41 +0200

    changement mineurs

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -30,7 +30,6 @@ const ScrollToTop = () => {
         <button
           onClick={scrollToTop}
           aria-label="Retourner en haut de la page"
-          // Ajout de "hidden lg:block" ici 👇
           className="hidden lg:block fixed bottom-6 right-6 z-50 p-3 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 transition-all duration-300 hover:scale-110 focus:outline-none focus:ring-2 focus:ring-blue-400 cursor-pointer"
         >
           <FaArrowUp size={20} />
